ddebdeadaf349e48a6548792241cb7cd8e3d7d1d
Android Studio <devandroid@ort>
2025-04-21 10:23:29 -0300

    rar

--- src/index.js
+++ src/index.js
@@ -10,10 +10,36 @@ app.get('/', (req, res) => {
 res.send('Ya estoy respondiendo!');
 })
 
-app.get('/saludar', (req, res) => { 
-res.send('Hello World!');
-})
+app.get('/saludar/:nombre', (req, res) => {
+    const { nombre } = req.params;
+    res.status(200).send(`Hola ${nombre}`);
+  });
 
-app.listen(port, () => {
-console.log(`Example app listening on port ${port}`)
-})
+
+app.get('/validarfecha/:ano/:mes/:dia', (req, res) => {
+    const { ano, mes, dia } = req.params;
+    
+    const fechaStr = `${ano}-${mes.padStart(2, '0')}-${dia.padStart(2, '0')}`;
+    const fecha = new Date(fechaStr);
+
+    if (isNaN(Date.parse(fechaStr))) {
+      return res.status(400).send('Fecha inválida');
+    }
+    
+    const fechaValida = 
+      fecha.getFullYear() == ano &&
+      (fecha.getMonth() + 1) == mes &&
+      fecha.getDate() == dia;
+    
+    if (!fechaValida) {
+      return res.status(400).send('Fecha inválida');
+    }
+    
+    res.status(200).send('Fecha válida');
+  });
+
+  
+
+  app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+  });
