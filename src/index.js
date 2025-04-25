e830765de958ef30fd23d338a2c0c60ab42140fb
Android Studio <devandroid@ort>
2025-04-25 12:09:42 -0300

    rar

--- src/index.js
+++ src/index.js
@@ -7,20 +7,24 @@ app.use(cors());
 app.use(express.json()); 
 
 app.get('/', (req, res) => {
-res.send('Ya estoy respondiendo!');
+  res.send('Ya estoy respondiendo!');
 })
 
 app.get('/saludar/:nombre', (req, res) => {
-    const { nombre } = req.params;
-    res.status(200).send(`Hola ${nombre}`);
+    console.log("hola");
+    const nombre  = req.params.nombre;
+    res.status(200).send(`Hola ${nombre}`);    
   });
 
 
 app.get('/validarfecha/:ano/:mes/:dia', (req, res) => {
-    const { ano, mes, dia } = req.params;
-    
+    const  ano = req.params.ano;
+    const mes = req.params.mes;
+    const dia = req.params.dia;
+
     const fechaStr = `${ano}-${mes.padStart(2, '0')}-${dia.padStart(2, '0')}`;
     const fecha = new Date(fechaStr);
+    
 
     if (isNaN(Date.parse(fechaStr))) {
       return res.status(400).send('Fecha inválida');
@@ -34,7 +38,6 @@ app.get('/validarfecha/:ano/:mes/:dia', (req, res) => {
     if (!fechaValida) {
       return res.status(400).send('Fecha inválida');
     }
-    
     res.status(200).send('Fecha válida');
   });
 
